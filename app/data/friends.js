38748c6af220bc0bd6e96f52f339188ac2da263d
Luis Morneno <morenoluise535@gmail.com>
2018-12-04 17:49:07 -0800

    add

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var friends = [
+    {
+      q1: "1",
+      q2: "2",
+      q3: "3",
+      q4: "4",
+      q5: "5",
+      q6: "1",
+      q7: "2",
+      q8: "3",
+      q9: "4",
+      q10: "5",
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,17 @@
+var path = require("path");
+
+
+module.exports = function(app) {
+
+    app.get("/servay", function(req, res) {
+      res.sendFile(path.join(__dirname, ""));
+    });
+  
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../../public/home.html"));
+      });
+
+    app.get("*", function(req, res) {
+      res.sendFile(path.join(__dirname, "../../public/home.html"));
+    });
+  };
